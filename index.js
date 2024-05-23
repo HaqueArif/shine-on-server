5ac2b7bf3f9ffa0b1a87b0d8000714d4726cde98
HaqueArif <haquearif143@gmail.com>
2024-05-23 08:56:30 +0600

    added client site url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3000/", credentials: true }));
+app.use(cors({ origin: "https://shine-on.vercel.app/", credentials: true }));
 app.use(express.json());
 
 // MongoDB Connection URL
